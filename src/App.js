883a1e3910aff0be16b3d3a90bed77d9738ec56a
joel irmiahu <joelirmiahu66@gmail.com>
2022-12-19 12:36:30 +0100

    feur

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import Header from './components/Header';
-import Footer from './components/Footer';
-import ExpenseItem from './components/ExpsenseItem';
+import ExpenseItem from './components/ExpenseItem';
 
 function App() {
   const expenses = [
