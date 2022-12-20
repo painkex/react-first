319c3e4dcfc8da38bc8cdea3dcd958e132c82a6e
joel irmiahu <joelirmiahu66@gmail.com>
2022-12-20 17:03:03 +0100

    listenings events

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -2,16 +2,19 @@ import React from "react";
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
-import "./Card.css";
 
 
-function ExpenseItem(props) {
+const ExpenseItem = (props) => {
+  const clickHandler = () => {
+    console.log("Clicked!");
+  };
   return (
     <Card className="expense-item">
         <ExpenseDate date={props.date}/>
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">{props.amount}$</div>
+        <button onClick={clickHandler}></button>
       </div>
     </Card>
   );
